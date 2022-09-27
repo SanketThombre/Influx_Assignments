85e7ffffad6fbe3a26da14add8d7034eed078973
sanket008 <sanket.p.thombare@gmail.com>
2022-09-27 14:30:05 +0530

    header file created

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+// import { Container, Navbar, Nav, Image } from 'react-bootstrap'; 
+import './All_Pages.css';
+
+export const Header = () => {
+    return (
+
+            <div className="header" >
+               <img src="https://www.fullstackpython.com/img/logos/react.png" width="100px" alt="React logo"/> 
+            <div className="userName">
+               
+                   
+                        <h6>John Wick</h6>
+                    <p>Developer</p>
+                            
+        </div>
+      </div>
+        
+    )
+}
